b568fa1f5d8016a69b01c97717e71f2623e9ae0f
itsunknown01 <aryamangohain@gmail.com>
2023-10-10 23:27:13 +0530

    Server sidebar
    server sidebar component is created

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -1,13 +1,54 @@
-import React from 'react'
+import { db } from "@/lib/db";
+import { userProfile } from "@/lib/user-profile";
+import { redirect } from "next/navigation";
+import React from "react";
+import ServerHeader from "./server-header";
 
 interface ServerSidebarProps {
-    serverId : string
+  serverId: string;
 }
 
-const ServerSidebar = ({serverId} : ServerSidebarProps) => {
+const ServerSidebar = async ({ serverId }: ServerSidebarProps) => {
+  const profile = await userProfile();
+
+  if (!profile) {
+    return redirect(`/`);
+  }
+
+  const server = await db.server.findUnique({
+    where: {
+      id: serverId,
+    },
+    include: {
+      channels: {
+        orderBy: {
+          createdAt: "asc",
+        },
+      },
+      members: {
+        include: {
+          profile: true,
+        },
+        orderBy: {
+          role: "asc",
+        },
+      },
+    },
+  });
+
+  if (!server) {
+    redirect(`/channels`)
+  }
+
+  const handleRole = server?.members.find(
+    (member) => member.profileId === profile.id
+  )?.role;
+
   return (
-    <div>ServerSidebar</div>
-  )
-}
+    <div className="flex flex-col h-full text-primary w-full dark:bg-[#282D31] bg-[#F2F3F5]">
+      <ServerHeader server={server} role={handleRole} />
+    </div>
+  );
+};
 
-export default ServerSidebar
+export default ServerSidebar;
